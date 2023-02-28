53f6566dced7e6e90c421a376de820165d5aef66
Ruan Finamor <ruanfinamor@gmail.com>
2023-02-28 18:51:07 -0300

    feat: delete button

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import { Editora } from './modelo/Editora'
 import './App.css';
 import Data from './components/Header';
 
-function App() {
-  const [editora, setEditora] = useState<Editora[]>([{
-    nome: 'Harry Potter',
-    codEditora: 1,
-  }]);
+function App() {  
 
   return (
     <div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,19 @@ import { useState } from 'react';
 import Table from 'react-bootstrap/esm/Table';
 import data from '../dados.json';
 
-const Data = () => {    
-  
+const Data = () => {
+
+  const handleDelete = (livrosId) => {
+    const newLivros = [...livros]
+    const index = livros.findIndex((livros) => livros.codEditora === livrosId);
+
+    newLivros.splice(index, 1);
+
+    setNewLivros(newLivros);
+  };
+
   const [livros, setLivros] = useState(data);
-  
+
   return (
 
     <Table striped bordered hover variant="dark">
@@ -21,15 +30,27 @@ const Data = () => {
       <tbody>
         {livros.map((livros) => (
           <tr>
-            <td>{livros.titulo}</td>
+            <td>
+              {livros.titulo}
+
+              <br/>
+
+              <button
+                type="button"
+                onClick={() => handleDelete(livros.codEditora)}
+              >
+                Excluir
+              </button>
+            </td>
             <td>{livros.resumo}</td>
             <td>{livros.editora}</td>
             <td>{livros.autores}</td>
           </tr>
+
         ))}
-      </tbody>        
+      </tbody>
     </Table>
   );
-}  
- 
+}
+
 export default Data;
